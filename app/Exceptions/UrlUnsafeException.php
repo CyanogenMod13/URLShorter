5a0cdde351ec256e466743d4aea2dd12f3d1ec52
CyanogenMod13 <sovkanbbb@gmail.com>
2022-05-25 12:10:50 +0300

    add text

--- app/Exceptions/UrlUnsafeException.php
+++ app/Exceptions/UrlUnsafeException.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace App\Exceptions;
 
 use Exception;
+use Throwable;
 
 class UrlUnsafeException extends Exception
 {
+	public function __construct(string $message = "The provided URL contains threatening content", int $code = 0, ?Throwable $previous = null)
+	{
+		parent::__construct($message, $code, $previous);
+	}
 }
