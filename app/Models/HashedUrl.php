51e5d505eb2bb2e6c8a818c8e12b790533935f26
CyanogenMod13 <sovkanbbb@gmail.com>
2022-05-25 12:13:09 +0300

    add properties

--- app/Models/HashedUrl.php
+++ app/Models/HashedUrl.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string hash
+ * @property string|null folder
+ * @property string originalUrl
+ */
 class HashedUrl extends Model
 {
     use HasFactory;
